a6d51fac44c04949dd462228c6bd6928de3ed839
Dante-Blasi <118856863+Dante-Blasi@users.noreply.github.com>
2023-10-17 19:34:13 -0400

    Final CSS3 site build

---
+++ CSS3/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <?php print '<!-- ***** $tableNum = ' . $tableNum . ' **** -->';
+    print '<h2>SITE ' . $tableNum . '</h2>';
+    ?>
+    <p><a href="<?php print '../index.php?time=' . $timeCurrent . '&page=' . PATH_PARTS['filename'] . '&table=' . $tableNum ?>">EXIT USING THIS LINK PLEASE</a></p>
+    <p><cite><a href="images/image-citations.txt">Image Sources</a></cite></p>
+</footer>
+</body>
+
+</html>

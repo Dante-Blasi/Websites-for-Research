808ee691c31a766b347942d8294fdf534d57ce97
Dante-Blasi <118856863+Dante-Blasi@users.noreply.github.com>
2023-10-17 19:37:10 -0400

    Final CSS8 site build

---
+++ CSS8/men.php
@@ -0,0 +1,43 @@
+<?php
+include 'top.php';
+?>
+
+<main>
+    <figure class="clothing item1">
+        <img alt="Mens Tshirt" src="images/mens-tshirt.jpg">
+        <figcaption>Basic Tee<br>
+            <del>$13.95</del> <span class="discount">$11.50</span>
+        </figcaption>
+    </figure>
+
+    <figure class="clothing item2">
+        <img alt="Mens Longsleeve Shirt" src="images/mens-longsleeve.jpg">
+        <figcaption>Crew Sweatshirt<br>
+            $22.50
+        </figcaption>
+    </figure>
+
+    <figure class="clothing item3">
+        <img alt="Mens Hooded Sweatshirt" src="images/mens-sweatshirt.jpg">
+        <figcaption>Classic Hoodie<br>
+            $32.95
+        </figcaption>
+    </figure>
+
+    <figure class="clothing item4">
+        <img alt="Mens Jeans" src="images/mens-jeans.jpg">
+        <figcaption>Blue Jeans<br>
+            <del>$40.50</del> <span class="discount">$24.50</span>
+        </figcaption>
+    </figure>
+
+    <figure class="clothing item5">
+        <img alt="Mens Khakis" src="images/mens-khakis.jpg">
+        <figcaption>Casual Khakis<br>
+            $44.95</figcaption>
+    </figure>
+</main>
+
+<?php
+include 'footer.php';
+?>
